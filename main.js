a84b255406b494e8134d8653a7a78e8814fa7551
Vladimir Tatarskiy <vtatarskiy@spbtv.com>
2022-06-14 09:55:57 +0300

    Init

---
+++ main.js
@@ -0,0 +1,30 @@
+
+import db_with_coords from "./db_with_coords.json" assert { type: "json" };
+
+window.addEventListener("load", onDocumentLoad);
+
+function onDocumentLoad() {
+  ymaps.ready(initYMaps)
+}
+
+function initYMaps() {
+  var myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 5
+  })
+  
+  prepareData()
+  
+  myMap.geoObjects
+  .add(
+    new ymaps.Placemark([55.684758, 37.738521], {
+      balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+    })
+  );
+}
+
+function prepareData() {
+  // var mydata = JSON.parse(db_with_coords);
+  console.log(db_with_coords)
+}
+
